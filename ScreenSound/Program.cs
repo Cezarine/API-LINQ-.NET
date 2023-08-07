13cc215d8ab9da2e964e310698d767efcb33565d
Cezarine <96452545+Cezarine@users.noreply.github.com>
2023-08-07 18:08:43 -0300

    Deserealizando Json

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound.Models;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
